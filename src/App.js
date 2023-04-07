3c3426038954f75e1a749c26f5a43832975e768d
sineTlsl <limsieun0313@gmail.com>
2023-04-07 11:46:09 +0900

    Feat: TodoList CRUD 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import TodoList from "./pages/TodoList";
+import TabNav from "./components/TabNav";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: ${(props) => props.theme.darkMode.background};
+`;
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1000px;
+  height: 600px;
+  background: ${(props) => props.theme.lightMode.background};
+  border-radius: 20px;
+
+  @media ${(props) => props.theme.tablet} {
+    width: 90%;
+    margin: 0 auto;
+  }
+`;
+
+const RowBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 520px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppContainer>
+        <Header />
+        <RowBox>
+          {/* <SideBar /> */}
+          <TodoList />
+        </RowBox>
+      </AppContainer>
+      <TabNav />
+    </Container>
   );
 }
 
